f98dd3746523503b054c3ce5c9edee12ba8508db
DCR <daniel.cruz.rosso@gmail.com>
2026-06-20 09:36:45 +0100

    feat: exercise swap + custom additions

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'dcr-gym-v5';
+const CACHE = 'dcr-gym-v6';
 const ASSETS = ['./index.html', './programs.js', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
