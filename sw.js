bb6287299afa970e8183586343bc4b6148953b5a
DCR <daniel.cruz.rosso@gmail.com>
2026-06-20 09:58:21 +0100

    style: adjust premium-action-pill padding and spacing, bump cache to dcr-gym-v9

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'dcr-gym-v8';
+const CACHE = 'dcr-gym-v9';
 const ASSETS = ['./index.html', './programs.js', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
