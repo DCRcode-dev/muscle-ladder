7480a6141a776a9d86ab045ba4f994a7a0973820
DCR <daniel.cruz.rosso@gmail.com>
2026-09-01 22:30:36 -0400

    feat(workout): active set focus spotlight, consistent prefill, live PR celebration, technique dossier & substitution overhaul (OTA-21)

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 
-const CACHE = 'dcr-gym-v25';
+const CACHE = 'dcr-gym-v26';
 const ASSETS = ['./index.html', './programs.js', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
