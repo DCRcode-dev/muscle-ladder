d9a0620a6d5053db29b34ca4866af7a9070017b6
DCR <daniel.cruz.rosso@gmail.com>
2026-09-01 22:08:39 -0400

    feat: luxury iPhone frosted masthead and resolve keyboard overlap glitch (OTA-17)

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 
-const CACHE = 'dcr-gym-v21';
+const CACHE = 'dcr-gym-v22';
 const ASSETS = ['./index.html', './programs.js', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
